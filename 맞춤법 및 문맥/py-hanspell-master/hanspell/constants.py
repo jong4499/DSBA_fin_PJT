437c85eabc4efe51af497985520f435290c05215
jong4499 <141287150+jong4499@users.noreply.github.com>
2023-11-15 23:19:59 +0900

    맞춤법교정 및 문법판단

---
+++ 맞춤법 및 문맥/py-hanspell-master/hanspell/constants.py
@@ -0,0 +1,10 @@
+base_url = 'https://m.search.naver.com/p/csearch/ocontent/util/SpellerProxy?passportKey=afe980d9db157babf9ac479bebd7b6ba14e52a07'
+
+
+class CheckResult:
+    PASSED = 0
+    WRONG_SPELLING = 1
+    WRONG_SPACING = 2
+    AMBIGUOUS = 3
+    STATISTICAL_CORRECTION = 4
+
